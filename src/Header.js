0e089c203b14bc5ecb5a0b08cf982781307ca46c
neechie <neechieg@gmail.com>
2018-01-23 15:52:18 -0500

    setup of header container

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => (
+  <nav className='header'>
+    <div className='container'>
+      <h2>WELCOME TO NBA MyPLAYER DATABASE</h2>
+    </div>
+  </nav>
+)
+
+export default Header
